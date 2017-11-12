ca847aad66aba16041259f30fed90ecd32f5fedb
zezoivanov <zezo@zezo.org>
2017-11-12 17:36:45 +0200

    Switched to Page Action

--- background.js
+++ background.js
@@ -2,12 +2,20 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.debugger.attach({tabId:tab.id}, version,
-      onAttach.bind(null, tab.id));
+chrome.pageAction.onClicked.addListener(function(tab) {
+  chrome.debugger.attach({tabId:tab.id}, version, onAttach.bind(null, tab.id));
 });
 
 var version = "1.0";
+
+function checkForValidUrl(tabId, changeInfo, tabInfo) {
+
+if (tabInfo.url.indexOf('virtualregatta.com') >= 0) {
+                chrome.pageAction.show(tabId);
+        }
+};
+
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
 
 function onAttach(tabId) {
   if (chrome.runtime.lastError) {
